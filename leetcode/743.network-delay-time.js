0d7cbe4c62aafd531d59bb7180634faf2c2e858c
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-07-25 18:36:58 +0200

    add Bellman-Ford solution to 743

--- leetcode/743.network-delay-time.js
+++ leetcode/743.network-delay-time.js
@@ -5,9 +5,9 @@
  */
 // @lc code=start
 /*
-T: O(NlogN)
-S: O(E + N)
-Technique: Greedy method + Dijkstra algorithm
+T: O(N * E)
+S: O(N)
+Technique: Bellman-Ford algorithm
 */
 /**
  * @param {number[][]} times
@@ -17,27 +17,20 @@ Technique: Greedy method + Dijkstra algorithm
  */
 var networkDelayTime = function (times, n, k) {
     const distances = new Array(n).fill(Infinity);
-    const adjacencyList = distances.map(() => []);
-    const queue = new FakePriorityQueue(k - 1);
+    let counter = n;
 
     distances[k - 1] = 0;
 
-    for (let [source, target, weight] of times) {
-        adjacencyList[source - 1].push([target - 1, weight]);
-    }
-
-    while (queue.size()) {
-        const currentVertex = queue.pop();
-        const adjacent = adjacencyList[currentVertex];
-
-        for (let [target, weight] of adjacent) {
-            const totalWeight = distances[currentVertex] + weight;
-
-            if (totalWeight < distances[target]) {
-                distances[target] = totalWeight;
-                queue.add(target);
+    while (counter > 0) {
+        for (let [source, target, weight] of times) {
+            const totalWeight = distances[source - 1] + weight;
+            
+            if (totalWeight < distances[target - 1]) {
+                distances[target - 1] = totalWeight;
             }
         }
+
+        counter--;
     }
 
     const max = Math.max(...distances);
@@ -46,25 +39,61 @@ var networkDelayTime = function (times, n, k) {
         ? -1
         : max;
 };
-
-// fake PQ implementation
-class FakePriorityQueue {
-    constructor(index) {
-        this.queue = [index];
-    }
-
-    add(index) {
-        this.queue.push(index);
-        this.queue.sort((a, b) => b - a);
-    }
-
-    pop() {
-        return this.queue.pop();
-    }
-
-    size() {
-        return this.queue.length;
-    }
-}
 // @lc code=end
+/*
+T: O(NlogN)
+S: O(E + N)
+Technique: Greedy method + Dijkstra algorithm
+*/
+// var networkDelayTime = function (times, n, k) {
+//     const distances = new Array(n).fill(Infinity);
+//     const adjacencyList = distances.map(() => []);
+//     const queue = new FakePriorityQueue(k - 1);
+
+//     distances[k - 1] = 0;
+
+//     for (let [source, target, weight] of times) {
+//         adjacencyList[source - 1].push([target - 1, weight]);
+//     }
+
+//     while (queue.size()) {
+//         const currentVertex = queue.pop();
+//         const adjacent = adjacencyList[currentVertex];
+
+//         for (let [target, weight] of adjacent) {
+//             const totalWeight = distances[currentVertex] + weight;
+
+//             if (totalWeight < distances[target]) {
+//                 distances[target] = totalWeight;
+//                 queue.add(target);
+//             }
+//         }
+//     }
+
+//     const max = Math.max(...distances);
+
+//     return max === Infinity
+//         ? -1
+//         : max;
+// };
+
+// // fake PQ implementation
+// class FakePriorityQueue {
+//     constructor(index) {
+//         this.queue = [index];
+//     }
+
+//     add(index) {
+//         this.queue.push(index);
+//         this.queue.sort((a, b) => b - a);
+//     }
+
+//     pop() {
+//         return this.queue.pop();
+//     }
+
+//     size() {
+//         return this.queue.length;
+//     }
+// }
 
