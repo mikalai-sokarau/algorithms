c56d01d710a64d5e1eeab05d9deea6c341374b79
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-10-18 22:42:28 +0200

    solve: 111

--- leetcode/111.minimum-depth-of-binary-tree.js
+++ leetcode/111.minimum-depth-of-binary-tree.js
@@ -13,12 +13,42 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+/*
+Complexity:
+T: O(n)
+S: O(n)
+*/
 /**
  * @param {TreeNode} root
  * @return {number}
  */
-var minDepth = function(root) {
-    
+var minDepth = function (root) {
+  if (!root) return root;
+
+  const queue = new Queue();
+  let depth = 1;
+
+  queue.enqueue(root);
+
+  while (queue.size()) {
+    for (let i = queue.size(); i > 0; i--) {
+      const node = queue.dequeue();
+
+      if (!node.left && !node.right) {
+        return depth;
+      }
+      if (node.left) {
+        queue.enqueue(node.left);
+      }
+      if (node.right) {
+        queue.enqueue(node.right);
+      }
+    }
+
+    depth++;
+  }
+
+  return depth;
 };
 // @lc code=end
 
