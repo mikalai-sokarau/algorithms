af5b08a8053fb0bef735f7ce74f4b52441836013
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-11-10 05:38:31 +0100

    solve: 290 (optimal)

--- leetcode/290.word-pattern.js
+++ leetcode/290.word-pattern.js
@@ -5,24 +5,36 @@
  */
 
 // @lc code=start
+/*
+Complexity:
+T: O(n)
+S: O(w + n)
+where
+w is number of words in s
+n is number of letters in pattern
+*/
 /**
  * @param {string} pattern
  * @param {string} s
  * @return {boolean}
  */
-var wordPattern = function(pattern, s) {
-  const map = {};
+var wordPattern = function (pattern, s) {
+  const keyValueMap = new Map();
+  const valueKeyMap = new Map();
   const words = s.split(' ');
 
+  if (words.length !== pattern.length) return false;
+
   for (let i = 0; i < words.length; i++) {
-    if (map[pattern[i]] && map[pattern[i]] !== words[i]) {
-      return false
-    } else {
-      map[pattern[i]] = words[i];
-    }
+    if (keyValueMap.has(pattern[i]) && keyValueMap.get(pattern[i]) !== words[i])
+      return false;
+    if (valueKeyMap.has(words[i]) && valueKeyMap.get(words[i]) !== pattern[i])
+      return false;
+
+    keyValueMap.set(pattern[i], words[i]);
+    valueKeyMap.set(words[i], pattern[i]);
   }
 
   return true;
 };
 // @lc code=end
-
