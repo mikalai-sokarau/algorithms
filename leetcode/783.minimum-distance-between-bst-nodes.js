e47d866b6b7631cebaa8ae2b968db5a1a16fa448
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-12-15 22:32:34 +0100

    solve: 783 (optimal)

--- leetcode/783.minimum-distance-between-bst-nodes.js
+++ leetcode/783.minimum-distance-between-bst-nodes.js
@@ -5,10 +5,8 @@
  */
 /*
 Complexity:
-T: O(n + m)
-S: O(m)
-where m is the max value in the tree
-n is the number of nodes in the tree
+T: O(n)
+S: O(1)
 */
 
 // @lc code=start
@@ -26,32 +24,21 @@ n is the number of nodes in the tree
  */
 var minDiffInBST = function (root) {
   let min = Infinity;
-  let nums = [];
+  let prev = Infinity;
 
   const dfs = (node) => {
-    if (node.left) {
-      nums[node.left.val] = node.left.val;
-      dfs(node.left);
-    }
-    if (node.right) {
-      nums[node.right.val] = node.right.val;
-      dfs(node.right);
-    }
+    if (!node) return;
+
+    dfs(node.right);
+
+    if (min > prev - node.val) min = prev - node.val;
+
+    prev = node.val;
+
+    dfs(node.left);
   };
 
   dfs(root);
-
-  nums[root.val] = root.val;
-  nums = nums.filter(Number.isInteger);
-
-  for (let i = 0; i < nums.length; i++) {
-    if (min === 1) {
-      break;
-    }
-    if (nums[i + 1] >= 0) {
-      min = Math.min(min, nums[i + 1] - nums[i]);
-    }
-  }
 
   return min;
 };
