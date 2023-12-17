d7d127872d7b760c3b9c0197190526dfb314d2d7
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-12-17 22:48:28 +0100

    solve: 645 (optimal)

--- leetcode/645.set-mismatch.js
+++ leetcode/645.set-mismatch.js
@@ -3,19 +3,30 @@
  *
  * [645] Set Mismatch
  */
-
+/*
+Complexity:
+T: O(n)
+S: O(1)
+*/
 // @lc code=start
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
 var findErrorNums = function (nums) {
+  for (let i = 0; i < nums.length; ) {
+    const curr = nums[i] - 1;
+
+    if (curr === i || nums[curr] === curr + 1) {
+      i++;
+    } else {
+      [nums[curr], nums[i]] = [nums[i], nums[curr]];
+    }
+  }
+
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== i + 1) {
       return [nums[i], i + 1];
-    }
-    if (nums[i] === nums[i + 1]) {
-      return [nums[i], nums[i] + 1];
     }
   }
 };
