07e1c4b5b9e8b47bde265f87c25ccd5ea51c87dd
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-12-27 23:29:52 +0100

    solve: 100

---
+++ leetcode/100.same-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/*
+Complexity:
+T: O(n)
+S: O(h) where h is the height of the tree
+*/
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function (p, q) {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
